1ca7a9d94df2735fdbee194b65e13c122daacdd8
Mathew Palomo <3533791150101@ingenieria.usac.edu.gt>
2024-01-31 20:37:59 -0600

    Actualizacion de opciones en switch

--- src/t1_calculadora_202300760/T1_Calculadora_202300760.java
+++ src/t1_calculadora_202300760/T1_Calculadora_202300760.java
@@ -11,32 +11,92 @@ public class T1_Calculadora_202300760 {
         System.out.println("Seleccione la operacion a realizar");
         System.out.println(" 1.Suma \n 2.Resta \n 3.Multiplicacion \n 4.Division \n 5.Tablas de multiplicar \n 6.Salir \n" );
         byte opcion = scanner.nextByte();
-        
-        //Leer los digitos para las operaciones
-        System.out.println("Ingrese dos digitos para realizar la operacion \nIngrese el primer digito");
-        double d1 = scanner.nextDouble();
-        System.out.println("Ingrese el segundo digito");
-        double d2 = scanner.nextDouble();
-        
+    
         //Declaracion de operadores 
-        double suma, resta, multiplicacion, division;
-        
-        //Generacion de operaciones}
-        suma = (d1+d2);
-        resta = (d1-d2);
-        multiplicacion = (d1*d2);
-        division = (d1/d2);
-        
+        double suma, resta, multiplicacion;
         
         switch (opcion){
             case 1:
+                
+            System.out.println("Ingrese el primer sumando: \n");
+            double suman1 = scanner.nextDouble();
+            System.out.println("Ingrese el segundo sumando: \n");
+            double suman2 = scanner.nextDouble();
+            suma = (suman1+suman2);
             System.out.println("El total de la suma es: "+suma);
+            break;
+            
             case 2:
+                
+                System.out.println("Ingrese el minuendo: \n");
+                double min = scanner.nextDouble();
+                System.out.println("Ingrese el sustraendo: \n");
+                double sust = scanner.nextDouble();
+                resta = (min-sust);
                 System.out.println("La diferencia de la resta es: "+resta);
+                break;
+                
             case 3:
+                
+                System.out.println("Ingrese el primer factor: \n");
+                double factor1 = scanner.nextDouble();
+                System.out.println("Ingrese el segundo factor: \n");
+                double factor2 = scanner.nextDouble();
+                multiplicacion = (factor1*factor2);
                 System.out.println("El producto de la multiplicacion es: "+multiplicacion);
+                break;
+                
             case 4:
-                System.out.println("EL cociente de la resta es: "+division);
+                
+                System.out.println("Ingrese el dividendo: ");
+                long dividendo = scanner.nextLong();
+                System.out.println("Ingrese el divisor: ");
+                long divisor = scanner.nextLong();
+                
+                if (divisor != 0){
+                long cociente = (dividendo/divisor);
+                long residuo = (dividendo%divisor);
+                System.out.println("EL cociente de la division es: "+cociente);
+                System.out.println("El residuo de la division es: "+residuo);
+                }
+                else{
+                    System.out.println("El divisor no puede ser ser 0");
+                            
+                    }
+                break;
+                
+            case 5:
+                
+                System.out.println("Ingrese el digito de la tabla a imprimir: ");
+                long tabla = scanner.nextLong();
+                
+                long inicio, fin;
+                
+                do{
+                System.out.println("Ingrese el inicio: ");
+                inicio = scanner.nextLong();
+                System.out.println("Ingrese el fin: ");
+                fin = scanner.nextLong();
+                
+                if(inicio>fin){
+                    System.out.println("Error de rango, ingrese nuevamente los datos");
+                    
+                }
+             }
+                while(inicio>fin);
+                System.out.println("Tabla de multiplicar del " + tabla + " desde " + inicio + " hasta " + fin + ":");
+                for(long i = inicio; i <= fin; i++) {
+                System.out.println(tabla + " x " + i + " = " + (tabla * i));
+                }
+                break;
+                
+            case 6:
+                
+                System.out.println("El usuario ha finalizado el programa desarrollado por: \nNombre: Mathew Daniel Palomo Carrillo\nCarnet: 202300760");
+                break;
+                          
+            default:
+                System.out.println("Opcion no valida");
         }
     }
 }
